4f2c2174b4fdcad98872b19e3cafd42b2f41122e
purpeliz <vladislav.nekrasov123@gmail.com>
2022-10-12 20:24:04 +0300

    Add project files.

---
+++ String Intern/Classes/TimerClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace String_Intern.Classes
+{
+    public class TimerClass
+    {
+        private double _timeElapsed;
+        private System.Timers.Timer _timer;
+
+        public double TimeElapsed
+        {
+            get { return _timeElapsed; }
+        }
+
+        public TimerClass()
+        {
+            _timeElapsed = 0;
+            _timer = new System.Timers.Timer(100);
+        }
+
+        public void Start()
+        {
+            SetTimer();
+        }
+
+        public void Stop()
+        {
+            _timer.AutoReset = false;
+            _timer.Enabled = false;
+            _timer.Stop();
+            _timeElapsed = 0;
+        }
+
+
+        private void SetTimer()
+        {
+            _timer.Elapsed += OnTimedEvent;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            _timeElapsed += 0.1;
+        }
+    }
+}
---
+++ String Intern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Timers;
+using System.Text;
+
+namespace Testing
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string s1 = "TestingTesting";
+            string s2 = new StringBuilder().Append("Testing").Append("Testing").ToString();
+            string s3 = string.Intern(s2);
+
+            DateTime start = DateTime.Now;
+            for (long i = 0; i < 10000000; i++)
+            {
+                if (s3 == s1)
+                    continue;
+            }
+            DateTime end = DateTime.Now;
+            Console.WriteLine($"s3 == s1: {end - start}");
+
+            start = DateTime.Now;
+            for (long i = 0; i < 10000000; i++)
+            {
+                if (s3 == string.IsInterned(s1))
+                    continue;
+            }
+            end = DateTime.Now;
+            Console.WriteLine($"s3 == s1 intern: {end - start}");
+        }
+    }
+}
